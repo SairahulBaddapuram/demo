a7a4fe0105a5275ff80becf8bb8a241f9d58fb54
SairahulBaddapuram <144637965+SairahulBaddapuram@users.noreply.github.com>
2024-01-30 14:55:21 -0500

    Delete ec2 directory

--- ec2/main.tf
+++
@@ -1,7 +0,0 @@
-resource "aws_instance" "ec2_instance" {
-    ami = var.ami_id
-    instance_type = var.instance_type
-    subnet_id = var.subnet
-    security_groups = [var.security_group]
-
-}
--- ec2/output.tf
+++
@@ -1,9 +0,0 @@
-output "ec2_id"{
-   description = "ec2"
-   value = aws_instance.ec2_instance.id
-   sensitive = false
- }
-
- output "ec2-arn" {
-   value = aws_instance.ec2_instance.arn
- }
